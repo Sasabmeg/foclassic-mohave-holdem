c43630759829ba1deeebd3250ad8c6909218de82
Sasabmeg <eeeeeeediot@gmail.com>
2022-12-06 02:45:02 +0100

    Added Fallouty font image files in different sizes. Added ini file for interface.

--- src/main/java/net/fodev/foclassic/holdem/Main.java
+++ src/main/java/net/fodev/foclassic/holdem/Main.java
@@ -1,7 +1,12 @@
 package net.fodev.foclassic.holdem;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.font.Rectangle;
+import com.jme3.math.ColorRGBA;
 import com.jme3.system.AppSettings;
+import net.fodev.foclassic.holdem.view.DrawBetValueText;
 import net.fodev.foclassic.holdem.view.GuiPicture;
 
 public class Main extends SimpleApplication {
@@ -28,6 +33,7 @@ public class Main extends SimpleApplication {
         try {
             init();
             initBackground();
+            initText();
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -36,14 +42,21 @@ public class Main extends SimpleApplication {
     private void init() {
         flyCam.setEnabled(false);
         setDisplayStatView(false);
-        //setDisplayFps(false);
+        setDisplayFps(false);
     }
 
     private void initBackground() {
-        GuiPicture background = new net.fodev.foclassic.holdem.view.GuiPicture("Background", assetManager, "images/table-mojave-holdem-sample.png", true);
+        GuiPicture background = new net.fodev.foclassic.holdem.view.GuiPicture("Background", assetManager, "images/background/table-mojave-holdem-base.png", true);
         background.setCenterPosition(width / 2, height / 2, -100);
         guiNode.attachChild(background);
     }
+
+    private void initText() {
+        BitmapFont fallouty_12 = assetManager.loadFont("fonts/Fallouty_12.fnt");
+        DrawBetValueText.drawTextBox(guiNode, fallouty_12, new Rectangle(404, height - 563, 85, 14), "18,760", BitmapFont.Align.Center);
+    }
+
+
 
     @Override
     public void simpleUpdate(float tpf) {
---
+++ src/main/java/net/fodev/foclassic/holdem/view/DrawBetValueText.java
@@ -0,0 +1,30 @@
+package net.fodev.foclassic.holdem.view;
+
+import com.jme3.font.BitmapFont;
+import com.jme3.font.BitmapText;
+import com.jme3.font.Rectangle;
+import com.jme3.math.ColorRGBA;
+import com.jme3.scene.Node;
+
+public class DrawBetValueText {
+    public static void drawText(Node parentNode, BitmapFont font, int x, int y, String text) {
+        BitmapText hudText = new BitmapText(font, false);
+        hudText.setSize(font.getCharSet().getRenderedSize());      // font size
+        hudText.setColor(ColorRGBA.fromRGBA255(98, 217, 62, 255));                             // font color
+        hudText.setText(text);             // the text
+        hudText.setLocalTranslation(x, y + hudText.getLineHeight(), 0); // position
+        parentNode.attachChild(hudText);
+    }
+
+    public static void drawTextBox(Node parentNode, BitmapFont font, Rectangle rectangle, String text, BitmapFont.Align align) {
+        BitmapText hudText = new BitmapText(font, false);
+        hudText.setSize(font.getCharSet().getRenderedSize());      // font size
+        hudText.setColor(ColorRGBA.fromRGBA255(98, 217, 62, 255));                             // font color
+        hudText.setText(text);             // the text
+        //hudText.setLocalTranslation(x, y + hudText.getLineHeight(), 0); // position
+        hudText.setBox(rectangle);
+        hudText.setAlignment(align);
+        hudText.setVerticalAlignment(BitmapFont.VAlign.Center);
+        parentNode.attachChild(hudText);
+    }
+}
